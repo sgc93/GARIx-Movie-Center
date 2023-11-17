f837763c6efa18b8b6794bb27189c31477e4fc3a
sgc93 <smachewgedefaw@gmail.com>
2023-11-17 08:17:01 -0900

    rename movie data properties

--- src/App.js
+++ src/App.js
@@ -1,60 +1,238 @@
 import { useState } from "react";
+import MovieDetail from "./Utilities/MovieDetail";
 import Header from "./components/Header/Header";
+import Searched from "./components/Searched/Searched";
 import SideBar from "./components/Sidebar/Sidebar";
 const tempMovieData = [
 	{
-		imdbID: "tt1375666",
-		Title: "Inception",
-		Year: "2010",
+		id: "tt1375666",
+		title: "Inception",
+		year: "2010",
+		rating: "9.0",
 		Poster:
 			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
 	},
 	{
-		imdbID: "tt0133093",
-		Title: "The Matrix",
-		Year: "1999",
+		id: "tt0133093",
+		title: "The Matrix",
+		year: "1999",
+		rating: "9.0",
 		Poster:
 			"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
 	},
 	{
-		imdbID: "tt6751668",
-		Title: "Parasite",
-		Year: "2019",
+		id: "tt6751668",
+		title: "Parasite",
+		year: "2019",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt1375666",
+		title: "Inception",
+		year: "2010",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+	},
+	{
+		id: "tt0133093",
+		title: "The Matrix",
+		year: "1999",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt6751668",
+		title: "Parasite",
+		year: "2019",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt1375666",
+		title: "Inception",
+		year: "2010",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+	},
+	{
+		id: "tt0133093",
+		title: "The Matrix",
+		year: "1999",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt6751668",
+		title: "Parasite",
+		year: "2019",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt1375666",
+		title: "Inception",
+		year: "2010",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+	},
+	{
+		id: "tt0133093",
+		title: "The Matrix",
+		year: "1999",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt6751668",
+		title: "Parasite",
+		year: "2019",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt1375666",
+		title: "Inception",
+		year: "2010",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+	},
+	{
+		id: "tt0133093",
+		title: "The Matrix",
+		year: "1999",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt6751668",
+		title: "Parasite",
+		year: "2019",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt1375666",
+		title: "Inception",
+		year: "2010",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+	},
+	{
+		id: "tt0133093",
+		title: "The Matrix",
+		year: "1999",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt6751668",
+		title: "Parasite",
+		year: "2019",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt1375666",
+		title: "Inception",
+		year: "2010",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+	},
+	{
+		id: "tt0133093",
+		title: "The Matrix",
+		year: "1999",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt6751668",
+		title: "Parasite",
+		year: "2019",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt1375666",
+		title: "Inception",
+		year: "2010",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+	},
+	{
+		id: "tt0133093",
+		title: "The Matrix",
+		year: "1999",
+		rating: "9.0",
+		Poster:
+			"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+	},
+	{
+		id: "tt6751668",
+		title: "Parasite",
+		year: "2019",
+		rating: "9.0",
 		Poster:
 			"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
 	},
 ];
 
-const tempWatchedData = [
-	{
-		imdbID: "tt1375666",
-		Title: "Inception",
-		Year: "2010",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-		runtime: 148,
-		imdbRating: 8.8,
-		userRating: 10,
-	},
-	{
-		imdbID: "tt0088763",
-		Title: "Back to the Future",
-		Year: "1985",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-		runtime: 116,
-		imdbRating: 8.5,
-		userRating: 9,
-	},
-];
 export default function App() {
+	const [detailId, setDetailId] = useState("tt0133093");
+	const [isDetailOpen, setIsDetailOpen] = useState(false);
 	const [movies] = useState(tempMovieData);
+
+	let movie;
+	if (isDetailOpen) {
+		tempMovieData.forEach((data) => {
+			if (data.id === detailId) {
+				movie = data;
+			}
+		});
+	}
+
+	function closeDetail() {
+		setIsDetailOpen((isDetailOpen) => !isDetailOpen);
+	}
+
+	function openDetail(id) {
+		setDetailId((detailId) => id);
+		setIsDetailOpen((isDetailOpen) => !isDetailOpen);
+	}
 
 	return (
 		<>
 			<Header movies={movies} />
-			{/* <Main tempWatchedData={tempWatchedData} movies={movies} /> */}
 			<SideBar />
+			<Searched movies={movies} openDetail={openDetail} />
+			{isDetailOpen && (
+				<MovieDetail
+					movie={movie}
+					isDetailOpen={isDetailOpen}
+					closeDetail={closeDetail}
+				/>
+			)}
 		</>
 	);
 }

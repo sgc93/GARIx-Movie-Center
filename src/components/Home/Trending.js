abc53933e14322ed16af52127510e70416502173
sgc93 <smachewgedefaw@gmail.com>
2023-11-19 10:24:47 -0900

    create contents for Trending movies

--- src/components/Home/Trending.js
+++ src/components/Home/Trending.js
@@ -1,5 +1,16 @@
-function Trending() {
-	return <div>Trending</div>;
+import MovieCard from "../../Utilities/MovieCard";
+
+function Trending({ movies }) {
+	return (
+		<div className="app__trending">
+			<h1 className="app__trending-title">Trending Movies</h1>
+			<ul className="app__trending-list">
+				{movies.map((movie) => (
+					<MovieCard movie={movie} />
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default Trending;

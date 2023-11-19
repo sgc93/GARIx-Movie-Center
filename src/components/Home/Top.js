666486c2d141923ce0fdd7246e156b79e1d0fc04
sgc93 <smachewgedefaw@gmail.com>
2023-11-19 10:24:24 -0900

    create contents for Top Rated movies

--- src/components/Home/Top.js
+++ src/components/Home/Top.js
@@ -1,5 +1,14 @@
-function Top() {
-	return <div>Top</div>;
-}
+import MovieCard from "../../Utilities/MovieCard";
 
+function Top({ movies }) {
+	return (
+		<div className="app__trending">
+			<ul className="app__trending-list">
+				{movies.map((movie) => (
+					<MovieCard movie={movie} />
+				))}
+			</ul>
+		</div>
+	);
+}
 export default Top;

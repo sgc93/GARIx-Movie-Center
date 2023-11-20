c0d01e099d865575588b3db12560f73c2f21a240
sgc93 <smachewgedefaw@gmail.com>
2023-11-20 04:55:37 -0900

    create suitable moviecard for api response

--- src/Utilities/MovieCard.js
+++ src/Utilities/MovieCard.js
@@ -1,5 +1,7 @@
-function MovieCard({ movie, openDetail }) {
-	return (
+function MovieCard({ movie, openDetail, tag }) {
+	return tag === 0 ? (
+		<MovieCard0 movie={movie} openDetail={openDetail} />
+	) : (
 		<li
 			key={movie.imdbID}
 			onClick={() => {
@@ -17,6 +19,29 @@ function MovieCard({ movie, openDetail }) {
 						{movie.Type === "movie" ? "🍿 Movie" : `📺 ${movie.Type}`}
 					</span>
 					<span>📅{movie.Year}</span>
+				</div>
+			</div>
+		</li>
+	);
+}
+
+function MovieCard0({ movie, openDetail }) {
+	return (
+		<li
+			key={movie.imdbID}
+			onClick={() => {
+				openDetail(movie.imdbID);
+			}}
+		>
+			<img
+				src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+				alt={`${movie.title} poster`}
+			/>
+			<div className="list__movie-data">
+				<h3>{movie.title}</h3>
+				<div>
+					<span>📅{movie.release_date}</span>
+					<span>{movie.popularity}</span>
 				</div>
 			</div>
 		</li>

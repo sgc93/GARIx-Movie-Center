66f798f8a15c836171a692ae15b8e7e04e02880c
sgc93 <smachewgedefaw@gmail.com>
2023-11-20 04:54:53 -0900

    handle fetching and displaying trending movies

--- src/components/Home/Trending.js
+++ src/components/Home/Trending.js
@@ -1,14 +1,57 @@
+import { useEffect, useState } from "react";
+import Error from "../../Utilities/Error";
+import Loading from "../../Utilities/Loading";
 import MovieCard from "../../Utilities/MovieCard";
 
-function Trending({ movies }) {
+const KEY = "988ba0f866b64552dd0b251b74c2b78d";
+
+function Trending({ movies, openDetail }) {
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState("");
+	const [top, setTop] = useState([]);
+
+	useEffect(function () {
+		setIsLoading(true);
+		async function fetchTop() {
+			try {
+				const response = await fetch(
+					`https://api.themoviedb.org/3/movie/popular?api_key=${KEY}`
+				);
+
+				if (!response.ok) {
+					throw new Error("something went wrong");
+				}
+				const data = await response.json();
+				setTop((top) => data.results);
+			} catch (err) {
+				setError(err.message);
+			} finally {
+				setIsLoading(false);
+			}
+		}
+
+		fetchTop();
+	}, []);
+
 	return (
 		<div className="app__trending">
 			<h1 className="app__trending-title">Trending Movies</h1>
-			<ul className="app__trending-list list">
-				{movies.map((movie) => (
-					<MovieCard movie={movie} />
-				))}
-			</ul>
+			{error && <Error error={error} />}
+			{isLoading && <Loading />}
+			{!error && !isLoading && (
+				<>
+					<ul className="app__trending-list list">
+						{top.map((movie) => (
+							<MovieCard
+								movie={movie}
+								openDetail={openDetail}
+								key={movie.id}
+								tag={0}
+							/>
+						))}
+					</ul>
+				</>
+			)}
 		</div>
 	);
 }

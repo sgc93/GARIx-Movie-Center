06671842591d4888794d786b93dabdad2dcf30ab
sgc93 <smachewgedefaw@gmail.com>
2023-11-20 05:40:29 -0900

    handle search result closing btn

--- src/components/Searched/Searched.js
+++ src/components/Searched/Searched.js
@@ -1,47 +1,33 @@
-import { useState } from "react";
 import Error from "../../Utilities/Error";
 import Loading from "../../Utilities/Loading";
 import MovieCard from "../../Utilities/MovieCard";
 import "./Searched.css";
 
-function Searched({ movies, openDetail, isLoading, error }) {
-	const [isOpen1, setIsOpen1] = useState(true);
+function Searched({ movies, openDetail, isLoading, error, setQuery }) {
 	return (
 		<div>
-			<div className={isOpen1 ? "main app__scrollbar-v" : "hidden"}>
+			<div className={"main app__scrollbar-v"}>
 				<div className="box">
 					<button
-						className="btn-toggle"
-						onClick={() => setIsOpen1((open) => !open)}
+						className={"btn-toggle hover"}
+						onClick={() => setQuery((query) => "")}
 					>
-						-
+						&times;
 					</button>
-					{isOpen1 && (
-						<>
-							{isLoading && <Loading />}
-							{!isLoading && !error && (
-								<ul className="result__list list">
-									{movies?.map((movie) => (
-										<>
-											<MovieCard
-												key={movie.imdbID}
-												movie={movie}
-												openDetail={openDetail}
-												isLoading={isLoading}
-											/>
-										</>
-									))}
-								</ul>
-							)}
-							{error && <Error error={error} />}
-						</>
+					{isLoading && <Loading />}
+					{error && <Error error={error} />}
+					{!isLoading && !error && (
+						<ul className="result__list list">
+							{movies?.map((movie) => (
+								<MovieCard
+									key={movie.imdbID}
+									movie={movie}
+									openDetail={openDetail}
+									isLoading={isLoading}
+								/>
+							))}
+						</ul>
 					)}
-					{/* {isOpen1 &&
-						{ isLoading && <Loading />}
-					{!isLoading && !error & (
-							
-					)}
-					 */}
 				</div>
 			</div>
 		</div>

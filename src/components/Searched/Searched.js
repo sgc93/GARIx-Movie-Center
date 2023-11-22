01c959c035133bd15046c1d394fe77eaf29ead05
sgc93 <smachewgedefaw@gmail.com>
2023-11-22 02:00:13 -0900

    add header tag for searched results with query

--- src/components/Searched/Searched.js
+++ src/components/Searched/Searched.js
@@ -3,10 +3,13 @@ import Loading from "../../Utilities/Loading";
 import MovieCard from "../../Utilities/MovieCard";
 import "./Searched.css";
 
-function Searched({ movies, openDetail, isLoading, error, setQuery }) {
+function Searched({ movies, openDetail, isLoading, error, query, setQuery }) {
 	return (
 		<div>
 			<div className={"main app__scrollbar-v"}>
+				<p className="header">
+					search results for <span>{query}</span>
+				</p>
 				<div className="box">
 					<button
 						className={"btn-toggle hover"}

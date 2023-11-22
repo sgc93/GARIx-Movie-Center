e634577d9835a055916d7e8beac6e040d42d67dc
sgc93 <smachewgedefaw@gmail.com>
2023-11-22 02:36:34 -0900

    handle categorical movie list fetching

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
 import { useEffect, useState } from "react";
 import MovieDetail from "./Utilities/MovieDetail";
+import Category from "./components/Category/Category";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import Searched from "./components/Searched/Searched";
 import SideBar from "./components/Sidebar/Sidebar";
-const tempMovieData = [
-	{
-		imdbID: "tt1375666",
-		Title: "Inception",
-		Year: "2010",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-	},
-	{
-		imdbID: "tt0133093",
-		Title: "The Matrix",
-		Year: "1999",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-	},
-	{
-		imdbID: "tt6751668",
-		Title: "Parasite",
-		Year: "2019",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-	},
-	{
-		imdbID: "tt1375666",
-		Title: "Inception",
-		Year: "2010",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-	},
-	{
-		imdbID: "tt0133093",
-		Title: "The Matrix",
-		Year: "1999",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
-	},
-	{
-		imdbID: "tt6751668",
-		Title: "Parasite",
-		Year: "2019",
-		Poster:
-			"https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
-	},
-];
 
 const KEY = "988ba0f866b64552dd0b251b74c2b78d";
 
@@ -62,6 +19,8 @@ export default function App() {
 
 	const [isLoading, setIsLoading] = useState(false);
 	const [error, setError] = useState("");
+
+	const [genre, setGenre] = useState("war");
 
 	useEffect(
 		function () {
@@ -148,7 +107,7 @@ export default function App() {
 	return (
 		<>
 			<Header movies={movies} query={query} onSearch={onSearch} />
-			<SideBar />
+			<SideBar setGenre={setGenre} />
 			{isDetailOpen && (
 				<MovieDetail
 					movie={movie}
@@ -171,6 +130,8 @@ export default function App() {
 						setQuery={setQuery}
 					/>
 				</>
+			) : genre ? (
+				<Category genre={genre} setGenre={setGenre} openDetail={openDetail} />
 			) : (
 				<Home openDetail={openDetail} />
 			)}

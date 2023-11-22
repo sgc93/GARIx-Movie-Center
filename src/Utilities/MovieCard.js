903c17d9189c4b2fcc83d178805c108d760b0bd1
sgc93 <smachewgedefaw@gmail.com>
2023-11-22 00:03:57 -0900

    update movie card contents for changed api results

--- src/Utilities/MovieCard.js
+++ src/Utilities/MovieCard.js
@@ -2,46 +2,31 @@ function MovieCard({ movie, openDetail, tag }) {
 	return tag === 0 ? (
 		<MovieCard0 movie={movie} openDetail={openDetail} />
 	) : (
-		<li
-			key={movie.imdbID}
-			onClick={() => {
-				openDetail(movie.imdbID);
-			}}
-		>
-			<img
-				src={movie.Poster === "N/A" ? "./logo.png" : movie.Poster}
-				alt={`${movie.Title} poster`}
-			/>
-			<div className="list__movie-data">
-				<h3>{movie.Title}</h3>
-				<div>
-					<span>
-						{movie.Type === "movie" ? "🍿 Movie" : `📺 ${movie.Type}`}
-					</span>
-					<span>📅{movie.Year}</span>
-				</div>
-			</div>
-		</li>
+		<MovieCard0 movie={movie} openDetail={openDetail} />
 	);
 }
 
 function MovieCard0({ movie, openDetail }) {
 	return (
 		<li
-			key={movie.imdbID}
+			key={movie.id}
 			onClick={() => {
-				openDetail(movie.imdbID);
+				openDetail(movie.id);
 			}}
 		>
 			<img
-				src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+				src={
+					movie.poster_path === null
+						? "./logo.png"
+						: `https://image.tmdb.org/t/p/original/${movie.poster_path}`
+				}
 				alt={`${movie.title} poster`}
 			/>
 			<div className="list__movie-data">
 				<h3>{movie.title}</h3>
 				<div>
 					<span>📅{movie.release_date}</span>
-					<span>{movie.popularity}</span>
+					<span>🍿{movie.popularity}</span>
 				</div>
 			</div>
 		</li>

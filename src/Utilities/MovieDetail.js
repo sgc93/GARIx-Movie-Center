318baca560eb553b0f5883359a60111f8cf0d638
sgc93 <smachewgedefaw@gmail.com>
2023-11-22 01:12:38 -0900

    add more detail about a selected movie

--- src/Utilities/MovieDetail.js
+++ src/Utilities/MovieDetail.js
@@ -2,19 +2,50 @@ import Loading from "./Loading";
 import "./MovieDetail.css";
 import RatingStar from "./RatingStar";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-function MovieDetail({ movie, closeDetail, isDetailLoading }) {
+const KEY = "eaf312c3";
+
+function MovieDetail({ movie, genres, closeDetail, isDetailLoading }) {
+	const [imdbID, setImdbId] = useState("");
+	const [imdbData, setImdbData] = useState({});
+
 	useEffect(
 		function () {
-			if (movie.Title) {
-				document.title = `GARIx | ${movie.Title}`;
+			if (movie.title) {
+				document.title = `GARIx | ${movie.title}`;
 			}
 			return () => {
 				document.title = "GARIx Movie Center";
 			};
 		},
 		[movie]
+	);
+
+	useEffect(
+		function () {
+			setImdbId(movie.imdb_id);
+		},
+		[movie]
+	);
+
+	useEffect(
+		function () {
+			async function fetchDetail() {
+				if (!imdbID) {
+					return;
+				}
+				const response = await fetch(
+					`http://www.omdbapi.com/?apikey=${KEY}&i=${imdbID}`
+				);
+				const data = await response.json();
+				setImdbData(data);
+				console.log(data);
+			}
+
+			fetchDetail();
+		},
+		[imdbID]
 	);
 
 	return (
@@ -24,21 +55,31 @@ function MovieDetail({ movie, closeDetail, isDetailLoading }) {
 			) : (
 				<>
 					{" "}
-					<img src={movie.Poster} alt={`${movie.Title} poster`} />
+					<img
+						src={
+							movie.poster_path === null
+								? "./logo.png"
+								: `https://image.tmdb.org/t/p/original/${movie.poster_path}`
+						}
+						alt={`${movie.title} poster`}
+					/>
 					<div className="about">
-						<h3 className="title">{movie.Title}</h3>
+						<h3 className="title">{movie.title}</h3>
 						<p className="rate__release">
-							<span className="release">📅 {movie.Released}</span>
-							<span className="duration">
-								{movie.Type === "movie"
-									? `🍿 ${movie.Runtime}`
-									: `📺${movie.totalSeasons} Seasons`}
-							</span>
-							<span className="rating">⭐ {movie.imdbRating}</span>
+							<span className="release">{`📅 ${movie.release_date}`}</span>
+							<span className="duration">{`🍿 ${imdbData.Runtime}`}</span>
+							<span className="rating"> {`⭐${imdbData.imdbRating}`}</span>
 						</p>
-						<p className="story">{movie.Plot}</p>
-						<p className="genre">{movie.Genre}</p>
-						<p className="casts"> {movie.Actors}</p>
+						<p className="story">{movie.overview}</p>
+						<div className="genres">
+							{genres &&
+								genres.map((genre) => (
+									<span className="genre" key={genre.id}>
+										{genre.name}
+									</span>
+								))}
+						</div>
+						<p className="casts"> {imdbData.Actors}</p>
 						<RatingStar movie={movie} maxRating={10} />
 					</div>
 					<div className="close">
